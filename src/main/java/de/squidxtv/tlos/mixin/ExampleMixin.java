9e7f807b02854e657db4cce060aa308b416586a8
SquidXTV <squidxtv@gmail.com>
2022-01-26 01:27:11 +0100

    Initial commit

---
+++ src/main/java/de/squidxtv/tlos/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package de.squidxtv.tlos.mixin;
+
+import de.squidxtv.tlos.TheLegendOfSteve;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TheLegendOfSteve.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
